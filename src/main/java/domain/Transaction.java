0500f4638b27c9b73487ceeb822162af705ca547
Jonathon <jonathon.mcguire.618@my.csun.edu>
2025-11-04 11:25:16 -0800

    Add timestamp field to Transaction class

--- src/main/java/domain/Transaction.java
+++ src/main/java/domain/Transaction.java
@@ -1,14 +1,18 @@
 package domain;
 
+import java.time.LocalDateTime;
+
 public class Transaction {
     private final TransactionType type;
     private final double amount;
     private final long cardNumber;
+    private final LocalDateTime timestamp;
 
-    public Transaction(TransactionType type, double amount, long cardNumber) {
+    public Transaction(TransactionType type, double amount, long cardNumber, LocalDateTime timestamp) {
         this.type = type;
         this.amount = amount;
         this.cardNumber = cardNumber;
+        this.timestamp = timestamp;
     }
 
     public enum TransactionType {
@@ -27,4 +31,7 @@ public class Transaction {
         return cardNumber;
     }
 
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
